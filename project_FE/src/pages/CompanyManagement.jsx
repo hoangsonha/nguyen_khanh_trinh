7c23e04351664d8dad0f236a7d0b0c295b28c865
hoangsonha <hoangsonhadev@gmail.com>
2025-05-13 14:29:59 +0700

    test

--- project_FE/src/pages/CompanyManagement.jsx
+++ project_FE/src/pages/CompanyManagement.jsx
@@ -113,7 +113,7 @@ function CompanyManagement() {
 
   return (
     <Container className="mt-4">
-      <h3 className="text-center fw-bold mb-4">Not found</h3>
+      <h3 className="text-center fw-bold mb-4">This feature is not available</h3>
     </Container>
   );
 }
